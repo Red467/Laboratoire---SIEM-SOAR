adb5621963e23f6fa1d0691256e4077510930100
Amon <81428061+Red467@users.noreply.github.com>
2023-10-25 14:42:49 +0200

    Add files via upload

---
+++ Laboratoire - SIEM SOAR/Question 1/labo1_Cloud/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_location" {
+  default     = "westeurope"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "win-vm-iis"
+  description = "Prefix of the resource name"
+}
